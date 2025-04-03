695734e9bb8d5f501db2abfd5804448936b58437
Sham <shamrdowaji@gmail.com>
2025-04-03 15:01:02 +0200

    added delete button

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 import ColorForm from "../ColorForm/ColorForm";
 import "./Color.css";
 
-export default function Color({ color, onUpdatedColor }) {
+export default function Color({ color, onUpdatedColor, onDeleteColor }) {
   const [editMode, setEditMode] = useState(false); // تتبع وضع التعديل
+  const [confirmDelete, setConfirmDelete] = useState(false);
 
   function handleEdit(updatedColor) {
     onUpdatedColor(updatedColor);
     setEditMode(false); // إغلاق وضع التعديل بعد الحفظ
+  }
+
+  function deleteColor() {
+    setConfirmDelete(true);
+  }
+  function confirmDeleteColor() {
+    onDeleteColor(color.id);
+    setConfirmDelete(false);
+  }
+  function cancelDelete() {
+    setConfirmDelete(false);
   }
 
   return (
@@ -23,7 +35,20 @@ export default function Color({ color, onUpdatedColor }) {
           <h4>{color.role}</h4>
           <p>Contrast: {color.contrastText}</p>
           <button onClick={() => setEditMode(true)}>Edit</button>
+          <button onClick={deleteColor}>Delete</button>
         </>
+      )}
+
+      {confirmDelete && (
+        <div className="delete-confirm-highlight">
+          <div>
+            <span>
+              <p className="color-card-highlight">Are you sure?</p>{" "}
+              <span></span> <button onClick={confirmDeleteColor}>Delete</button>
+              <button onClick={cancelDelete}>Cancel</button>
+            </span>
+          </div>
+        </div>
       )}
     </div>
   );
